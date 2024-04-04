bf98c4c9a2e8c629d08c5d418db058eec4ec196e
Pramodjena <pramoodjena@gmail.com>
2024-04-04 08:59:05 +0530

    control_flow file added

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,26 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+
